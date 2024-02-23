9906c1086d559a3d97d3130981f871be807a4127
diego <diego.cruzov@ingenieria.unam.edu>
2024-02-23 13:39:00 -0600

    Initial commit

---
+++ app/src/main/java/com/mexiti/listacomidaor/ui/theme/Type.kt
@@ -0,0 +1,61 @@
+package com.mexiti.listacomidaor.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.mexiti.listacomidaor.R
+
+val AbrilFatFace = FontFamily(
+    Font (R.font.abrilfatface_regular)
+)
+val Monserrat = FontFamily(
+    Font(R.font.montserrat_bold),Font(R.font.montserrat_regular)
+)
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = AbrilFatFace,
+        fontWeight = FontWeight.Normal,
+        fontSize = 36.sp,
+    ),
+
+    displayMedium = TextStyle(
+        fontFamily = Monserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+
+    labelSmall = TextStyle(
+        fontFamily = Monserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    ),
+
+    bodyLarge = TextStyle(
+        fontFamily = Monserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    )
+
+
+    /* Other default text styles to override
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+    */
+)
